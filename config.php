874bbe2ed08a16fcd6ed9726edd7cc75233b6d01
SRIRAM K <sriramnagai06@gmail.com>
2026-07-26 21:44:03 +0530

    Final production setup and cleanup

--- config.php
+++ config.php
@@ -6,9 +6,9 @@
  * Location: /config.php
  */
 
-// ---- Error reporting ----
+// ---- Error reporting (hide errors in production) ----
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 
 // ---- Security Headers & HTTPS Enforcement ----
